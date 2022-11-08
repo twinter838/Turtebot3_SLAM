aedf4e6ea5c12c2c31a15dbddb762a9246cdeae6
Theo <tdwinter@wpi.edu>
2022-11-07 20:37:24 -0500

    Work on Lab 2

--- Lab2/src/lab2.py
+++ Lab2/src/lab2.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+import math
 import rospy
 from nav_msgs.msg import Odometry
 from geometry_msgs.msg import PoseStamped
@@ -15,15 +16,22 @@ class Lab2:
         ### REQUIRED CREDIT
         ### Initialize node, name it 'lab2'
         # TODO
+        self.py=0
+        self.px=0
+        self.pth=0
+        rospy.init_node('Lab2')
         ### Tell ROS that this node publishes Twist messages on the '/cmd_vel' topic
+        self.pub_cmd_vel = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
         # TODO
         ### Tell ROS that this node subscribes to Odometry messages on the '/odom' topic
         ### When a message is received, call self.update_odometry
+        rospy.Subscriber('/odom', Odometry , self.update_odometry)
         # TODO
         ### Tell ROS that this node subscribes to PoseStamped messages on the '/move_base_simple/goal' topic
         ### When a message is received, call self.go_to
         # TODO
-        pass # delete this when you implement your code
+        rospy.Subscriber('/move_base_simple/goal', PoseStamped, self.go_to)
+        # delete this when you implement your code
 
 
 
@@ -33,12 +41,18 @@ class Lab2:
         :param linear_speed  [float] [m/s]   The forward linear speed.
         :param angular_speed [float] [rad/s] The angular speed for rotating around the body center.
         """
-        ### REQUIRED CREDIT
-        ### Make a new Twist message
-        # TODO
-        ### Publish the message
-        # TODO
-        pass # delete this when you implement your code
+        msg_cmd_vel = Twist()
+        # Linear velocity
+        msg_cmd_vel.linear.x = linear_speed
+        msg_cmd_vel.linear.y = 0.0
+        msg_cmd_vel.linear.z = 0.0
+        # Angular velocity
+        msg_cmd_vel.angular.x = 0.0
+        msg_cmd_vel.angular.y = 0.0
+        msg_cmd_vel.angular.z = angular_speed
+        # Send command
+        self.pub_cmd_vel.publish(msg_cmd_vel)
+
 
     
         
@@ -49,7 +63,18 @@ class Lab2:
         :param linear_speed [float] [m/s] The forward linear speed.
         """
         ### REQUIRED CREDIT
-        pass # delete this when you implement your code
+        distanceOld=math.sqrt(math.pow(self.px,2)+math.pow(self.py,2))
+        oldX=self.px
+        oldY=self.py
+        print("Starting Movement")
+        print(abs(math.sqrt(math.pow(self.px,2)+math.pow(self.py,2))))
+        print(distance)
+        while(abs(math.sqrt(math.pow(self.px-oldX,2)+math.pow(self.py-oldY,2)))<distance):
+            self.send_speed(linear_speed,0)
+            # print(abs(math.sqrt(math.pow(self.px,2)+math.pow(self.py,2))-distanceOld))
+            rospy.sleep(0.1)
+        print("Stopping Movement")
+        self.send_speed(0,0)
 
 
 
@@ -60,9 +85,32 @@ class Lab2:
         :param angular_speed [float] [rad/s] The angular speed.
         """
         ### REQUIRED CREDIT
-        pass # delete this when you implement your code
+        angleOld=self.pth
+        angle=self.normalize_angle(angle+angleOld)
+        while abs(self.pth-angle) > 0.1:
+            angle=self.normalize_angle(angle)
+            if(angle>angleOld):
+                print('Turning Left')
+                self.send_speed(0,aspeed)
+            if(angle<angleOld):
+                print("Turning Right")
+                self.send_speed(0,-aspeed)
+            rospy.sleep(0.1)
+        self.send_speed(0,0)
+
+    def normalize_angle_positive(self,angle):
+        """ Normalizes the angle to be 0 to 2*pi
+        It takes and returns radians. """
+        return math.fmod(math.fmod(angle, 2.0*math.pi) + 2.0*math.pi, 2.0*math.pi)
 
 
+    def normalize_angle(self,angle):
+        """ Normalizes the angle to be -pi to +pi
+        It takes and returns radians."""
+        a = self.normalize_angle_positive(angle)
+        if a > math.pi:
+            a -= 2.0 *math.pi
+        return a
 
     def go_to(self, msg):
         """
@@ -71,7 +119,30 @@ class Lab2:
         :param msg [PoseStamped] The target pose.
         """
         ### REQUIRED CREDIT
-        pass # delete this when you implement your code
+        waypointX=  msg.pose.position.x
+        waypointY = msg.pose.position.y
+        quat_orig = msg.pose.orientation
+        quat_list = [quat_orig.x, quat_orig.y, quat_orig.z, quat_orig.w]
+        (roll , pitch , yaw) = euler_from_quaternion(quat_list)
+        waypointYaw = yaw
+        print(waypointX)
+        print(waypointY)
+        print(waypointYaw)
+        distance=math.sqrt(math.pow(self.px-waypointX,2)+math.pow(self.py-waypointY,2))
+        print(distance)
+        yawAngle=math.atan2(waypointY-self.py,waypointX-self.px)
+        print("Rotating")
+        print(yawAngle)
+        self.rotate(yawAngle,1)
+        rospy.sleep(2)
+        print('Driving')
+        print(distance)
+        self.drive(distance,0.5)        
+        self.rotate(waypointYaw-self.pth,1)
+        if(abs(waypointX-self.px)>0.3 and abs(waypointY-self.py>0.3)):
+            print("Running Recursively to Correct Error")
+            self.go_to(msg)
+        
 
 
 
@@ -83,7 +154,12 @@ class Lab2:
         """
         ### REQUIRED CREDIT
         # TODO
-        pass # delete this when you implement your code
+        self.px = msg.pose.pose.position.x
+        self.py = msg.pose.pose.position.y
+        quat_orig = msg.pose.pose.orientation
+        quat_list = [quat_orig.x, quat_orig.y, quat_orig.z, quat_orig.w]
+        (roll , pitch , yaw) = euler_from_quaternion(quat_list)
+        self.pth = yaw
 
 
 
@@ -111,7 +187,13 @@ class Lab2:
 
 
     def run(self):
-        rospy.spin()
+        rospy.sleep(0.5)
+        # while not rospy.is_shutdown():
+            
+        #     rospy.sleep(0.1)
 
+        #     print("Pose X="+self.px)
+        #     print("Pose Y="+self.py)
+        rospy.spin()
 if __name__ == '__main__':
     Lab2().run()
